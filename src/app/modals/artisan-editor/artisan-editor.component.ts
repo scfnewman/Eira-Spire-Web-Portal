6ef07016be4d726c832ec402cf539885fd885387
Simon Newman <simon@ineedsurgery.com>
2023-03-28 16:40:52 +0100

    Artisan editor

--- src/app/modals/artisan-editor/artisan-editor.component.ts
+++ src/app/modals/artisan-editor/artisan-editor.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit, Input } from '@angular/core';
+import { ModalController, PopoverController } from '@ionic/angular';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
 	selector: 'app-artisan-editor',
@@ -8,11 +10,23 @@ import { ModalController } from '@ionic/angular';
 })
 export class ArtisanEditorModal implements OnInit {
 
+	@Input() Data: any;
+
+	ArtisanData: FormGroup;
 	Error: boolean = false;
 
 	constructor(
-		private _ModalCtrl: ModalController
-	) { }
+		private _ModalCtrl: ModalController,
+		private _FormBuilder: FormBuilder,
+		private _DataService: DataService
+	) {
+		this.ArtisanData = this._FormBuilder.group({
+			Name: new FormControl((this.Data && this.Data.Name) ? this.Data.Name : null, Validators.required),
+			Form: new FormControl((this.Data && this.Data.Form) ? this.Data.Form : null, Validators.required),
+			Type: new FormControl((this.Data && this.Data.Type) ? this.Data.Type : null, Validators.required),
+			Description: new FormControl((this.Data && this.Data.Description) ? this.Data.Description : null, Validators.required),
+		})
+	}
 
 	ngOnInit() { }
 
