5f58c038c251957f6d2db8bde670d9a41994c768
Simon Newman <simon@ineedsurgery.com>
2023-03-28 16:40:15 +0100

    Artisan page

--- src/app/pages/artisan/artisan.page.ts
+++ src/app/pages/artisan/artisan.page.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { ModalController, PopoverController } from '@ionic/angular';
+
+import { OptionsPopoverComponent } from 'src/app/components/options-popover/options-popover.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { ArtisanEditorModal } from 'src/app/modals/artisan-editor/artisan-editor.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-artisan',
-  templateUrl: './artisan.page.html',
-  styleUrls: ['./artisan.page.scss'],
+	selector: 'app-artisan',
+	templateUrl: './artisan.page.html',
+	styleUrls: ['./artisan.page.scss'],
 })
 export class ArtisanPage implements OnInit {
 
-  constructor() { }
+	constructor(
+		public _DataService: DataService,
+		public _ModalCtrl: ModalController,
+		public _PopCtrl: PopoverController,
+		public _AuthService: AuthService,
+		public _DatePipe: DatePipe
+	) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	async OpenEditor() {
+		const Modal = await this._ModalCtrl.create({
+			component: ArtisanEditorModal
+		})
+		return await Modal.present();
+	}
+
+	async OpenOptions(_Event: Event, _Data) {
+		const Pop = await this._PopCtrl.create({
+			component: OptionsPopoverComponent,
+			event: _Event,
+			componentProps: {
+				"Type": "Potion",
+				"Data": _Data
+			}
+		})
+
+		return await Pop.present();
+	}
+
 
 }
